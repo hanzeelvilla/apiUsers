135f4be6468694427f17cb66392a40df7516a9c6
Hanzeel Villa <hanzeelvilla0@gmail.com>
2024-09-02 23:14:22 -0600

    GET

--- controllers/users.js
+++ controllers/users.js
@@ -10,5 +10,19 @@ module.exports = {
         })
         .then(user => res.status(201).send(user))
         .catch(error => res.status(400).send(error))
-    }
+    },
+    list(req, res) {
+        return User.findAll({})
+        .then(user => res.status(200).send(user))
+        .catch(error => res.status(400).send(error))
+    },
+    find(req, res) {
+        return User.findAll({
+            where: {
+                id: req.params.id,
+            }
+        })
+        .then(user => res.status(200).send(user))
+        .catch(error => res.status(400).send(error))
+    }   
 }
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,14 @@
 const usersController = require("../controllers/users")
 const router = require("express").Router()
 
+router.get("/", (req, res) => {
+    usersController.list(req, res)
+})
+
+router.get("/:id", (req, res) => {
+    usersController.find(req, res)
+})
+
 router.post("/", (req, res) => {
     usersController.create(req, res)
 })
